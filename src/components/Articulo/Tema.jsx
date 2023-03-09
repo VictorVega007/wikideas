27950015057734fd27f99791cbd84f47a4324344
adrianaps <tecnologianormal@hotmail.com>
2023-03-09 11:41:57 -0500

    "Vista de temas"

--- src/components/Articulo/Tema.jsx
+++ src/components/Articulo/Tema.jsx
@@ -2,7 +2,8 @@ import { getTopicById } from '../../shared/service';
 import { useEffect, useState} from 'react';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Item from '@mui/material/Grid';
-import { Typography, Box, Stack, Chip} from '@mui/material';
+import { Typography, Box, Stack, Chip, Avatar, Icon} from '@mui/material';
+import {} from '@mui/icons-material/';
 import {} from './tema.css';
 
 export const Tema = () =>{
@@ -13,10 +14,12 @@ export const Tema = () =>{
     const [tags, setTags] = useState(null);
     const [author, setAuthor] = useState(null);
     const [date, setDate] = useState(null);
+    const [dateUpdate, setDateUpdate] = useState(null);
+    const [category, setCategory] = useState(null);
  
     useEffect(() => {
         const getTheme = async () => {
-            const id = 7;
+            const id = 10;
             const response = await getTopicById(id);
             const title = response.title;
             const description = response.description;
@@ -24,55 +27,68 @@ export const Tema = () =>{
             const tags = response.tags;
             const author = response.author;
             const date = response.publishDate;
+            const updateDate = response.updateDate;
+            const category = response.category.title;
             setTitle(title);
             setDescription(description);
             setUrl(url);
             setTags(tags);
             setAuthor(author);
             setDate(date);
+            setDateUpdate(updateDate);
+            setCategory(category);
         };
         getTheme();
     }, []);
 
+    let dateUpd;
+    if(dateUpdate != undefined){
+        dateUpd = <Typography variant="caption">{`Modificado el ${new Date(dateUpdate).toLocaleDateString("es-cl")}`}</Typography>
+    }
+
     return (
         <div>
-            <Grid container spacing={0.5} display = {'flex'}>
-                <Grid xs={7} className='contenido' marginLeft={5}>
+            <Grid container spacing={0.5} padding={5}>
+                <Grid xs={3}>Temas por Categoría</Grid>
+                <Grid xs={5.5}>
                     <Box display='flex' flexDirection='column'>
                         <Item>
-                            <Typography variant="h3" noWrap overflow="visible">
+                            <Typography variant="h4" noWrap overflow="visible" padding={5}>
                                 {title}
                             </Typography>
                         </Item>
-                        <Box boxShadow={3} width="100%" overflow="hidden">
+                        <Box boxShadow={3} width="100%" overflow="hidden" className="contenido">
                             <Item>
-                                <Typography variant="body1" overflow="visible" textAlign="justify" marginBottom={10}>
+                                <Typography variant="body1" overflow="visible" textAlign="justify" padding={5}>
                                     {description}
                                 </Typography>
                             </Item>
-                            <Item>
-                                <Typography variant="body1" className='author'>
-                                    Publicada por: {author} el {date}
-                                </Typography>
-                                <Typography variant="body1" className='author'>
-                                    Modificada por: {author} 
-                                </Typography>
-                    </Item>
+                            <Item marginBottom={5} display={"flex"} alignItems={"center"} justifyContent={"center"}>
+                                {
+                                    <Stack direction="row" spacing={1}>
+                                        {tags?.map(e=>(
+                                            <Chip label={e} className="tags"></Chip>
+                                        ))}
+                                    </Stack>
+                                }
+                            </Item>
                         </Box>
                     </Box>
                 </Grid>
-                <Grid xs={4} marginTop={13} marginLeft={10}>
-                    <Item>
-                        <img src={url} className="img-theme"></img>
+                <Grid xs={2.5} marginTop={15}>
+                    <Item marginLeft={5} className="img-theme">
+                        <img src={url} ></img>
                     </Item>
-                    <Item className='container-tags'>
-                        {
-                            <Stack direction="row" spacing={4}>
-                                {tags?.map(e=>(
-                                    <Chip label={e}></Chip>
-                                ))}
-                            </Stack>
-                        }
+                    <Item height={50}>
+                        <Typography variant="body1" overflow="visible" textAlign="justify" marginTop={5} marginLeft={5}>
+                            {`Categoría: ${category}`}   
+                        </Typography>
+                    </Item>
+                    <Item marginLeft={5}>
+                        <Typography variant="caption" display={"block"} paddingBottom={2}>
+                        {`Publicado por: ${author} el ${new Date(date).toLocaleDateString("es-cl")}`}
+                        </Typography>
+                        {dateUpd}
                     </Item>
                     
                 </Grid>
